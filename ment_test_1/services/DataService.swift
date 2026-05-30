6a0223236602546020645090364033e46e255b0a
Alex-Yu21 <yualex2112@gmail.com>
2026-05-30 18:02:26 +0200

    feat: add DataService with loadRates and loadTransactions, setup TableViewController in Storyboard

--- ment_test_1/services/DataService.swift
+++ ment_test_1/services/DataService.swift
@@ -8,11 +8,74 @@ import Foundation
 
 struct DataService {
     func loadRates() -> [RateModel] {
-        guard let url = Bundle.main.url(forResource: "rates", withExtension: "plist" )
-        else {  return []
+        guard let url = Bundle.main.url(forResource: "rates", withExtension: "plist")
+        else { return []
         }
+        
+        guard let data = try? Data(contentsOf: url)
+        else {
+            return []
+        }
+        
+        guard let array = try? PropertyListSerialization.propertyList(from: data, format: nil) as? [[String: Any]]
+        else {
+            return []
+        }
+        
+        var rates: [RateModel] = []
+
+        for dictionary in array {
+            guard let rateString = dictionary["rate"] as? String,
+                    let rate = Double(rateString),
+                    let from = dictionary["from"] as? String,
+                    let to = dictionary["to"] as? String
+              else {
+                  continue
+              }
+            let rateModel = RateModel(from: from, to: to, rate: rate)
+            
+            rates.append(rateModel)
+        }
+        
+        return rates
+
+        
     }
+    
+    func loadTransactions() -> [TransactionModel] {
+        guard let url = Bundle.main.url(forResource: "transactions", withExtension: "plist")
+        else { return []
+        }
+        
+        guard let data = try? Data(contentsOf: url)
+        else {
+            return []
+        }
+        
+        guard let array = try? PropertyListSerialization.propertyList(from: data, format: nil) as?
+                [[String: Any]]
+        else {
+            return []
+        }
+        
+        var transactions: [TransactionModel] = []
+
+        for dictionary in array {
+            guard let amountString = dictionary["amount"] as? String,
+                    let amount = Double(amountString),
+                    let currency = dictionary["currency"] as? String,
+                    let sku = dictionary["sku"] as? String
+              else {
+                  continue
+              }
+            let transactionModel = TransactionModel(amount: amount, currency: currency, sku: sku)
+            
+            transactions.append(transactionModel)
+        }
+        
+        return transactions
+
+    }
+    
 }
   
-
-
